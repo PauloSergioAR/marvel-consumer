cd57e2494a7f9491ba77814fd970801fded95247
Paulo Ribeiro <paulo.sergio.ar@gmail.com>
2021-04-25 23:01:23 -0300

    Adjusting documentation

--- swagger.js
+++ swagger.js
@@ -3,4 +3,14 @@ const swaggerAutogen = require('swagger-autogen')();
 const outputFile = './swagger_output.json';
 const endpointFiles = ['./src/routes.js'];
 
-swaggerAutogen(outputFile, endpointFiles)
+const doc = {
+    info: {
+        version: "1.0.0",
+        title: "Marvel Adapter Api",
+        description: "Adapter for the Marvel api"
+    },
+    host: "https://warm-hamlet-90772.herokuapp.com",
+    schemes:['https']
+}
+
+swaggerAutogen(outputFile, endpointFiles, doc);
